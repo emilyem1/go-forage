0ed86fdda6c4043169914cff958472c5e8e50432
Mohamed Waked <mwaked01@gmail.com>
2024-01-22 03:48:30 -0500

    style blog list item  ocmponent

--- frontend/src/components/BlogListItem.jsx
+++ frontend/src/components/BlogListItem.jsx
@@ -1,18 +1,18 @@
-
+import "../styles/MushroomListItem.scss";
 
 const BlogListItem = (props) => {
   const { blog } = props;
 
   return (
-    <section >
+    <section className="mushroom-list__item">
       <div >MAP AT LAT:{blog.lat}, LONG:{blog.long}</div>
       <div >{blog.title}</div>
       <div >By: {blog.username}</div>
-      <img src={`images/${blog.mushroom_image}`} alt={blog.mushroom} />
-      <div >
-        <footer >
+      <img className="mushroom-list__image" src={`images/${blog.mushroom_image}`} alt={blog.mushroom} />
+      <div className="mushroom-list__details">
+        <footer className="mushroom-list__info">
           <div >{blog.mushroom}</div>
-          <div >
+          <div className="mushroom-list__description">
             {blog.content} 
           </div>
         </footer>

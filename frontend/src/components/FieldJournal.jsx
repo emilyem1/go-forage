d3ccbac2d16465e1f66b37027c0e14e4850842d5
emilyem1 <emilyemcmahonn@gmail.com>
2024-01-25 18:40:58 -0800

    Add FieldJournal component

---
+++ frontend/src/components/FieldJournal.jsx
@@ -0,0 +1,13 @@
+import FieldJournalItem from "./FieldJournalItem";
+
+const FieldJournal = (props) => {
+  return (
+    <main>
+      <p>Users field journal will live here</p>
+      <p>Ability to see all past enteries + make new one</p>
+      <FieldJournalItem />
+    </main>
+  );
+};
+
+export default FieldJournal;
---
+++ frontend/src/components/FieldJournalItem.jsx
@@ -0,0 +1,10 @@
+
+const FieldJournalItem = () => {
+  return (
+    <section className="mushroom-list__item">
+      <p>📖 I'm the FieldJournalItem</p>
+    </section>
+  );
+};
+
+export default FieldJournalItem;

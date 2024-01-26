870ed0dfbfaa3e48318e1606f39bf875c4f5ce77
Kenny Lee <kenny.lee927@gmail.com>
2024-01-26 01:34:03 -0500

    add form logic and handlers for login or signup

--- frontend/src/components/LoginSignup.jsx
+++ frontend/src/components/LoginSignup.jsx
@@ -1,29 +1,80 @@
 import "../styles/LoginSignup.scss";
+import { useState } from "react";
 
 const LoginSignup = (props) => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+    action: "login", // default to login
+  });
+
+  const [signupData, setSignupData] = useState({
+    email: "",
+    password: "",
+    fullname: "", // for signup only
+    action: "signup", // default to signup
+  });
+
+  const handleInputChange = (form, e) => {
+    const formData = form === "login" ? loginData : signupData;
+    const setFormData = form === "login" ? setLoginData : setSignupData;
+
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFormSubmit = async (form, e) => {
+    e.preventDefault();
+    const formData = form === "login" ? loginData : signupData;
+
+    try {
+      const response = await fetch("http://localhost:3000/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <main>
-      <div class="formContainer">
+      <div className="formContainer">
         {/* Login Form */}
-        <form action="" method="POST" id="login-form" class="form">
+        <form
+          onSubmit={(e) => handleFormSubmit("login", e)}
+          id="login-form"
+          className="form"
+        >
           <h2>Login</h2>
-          <label for="email">E-mail Address:</label>
+          <label htmlFor="email">E-mail Address:</label>
           <input
+            onChange={(e) => handleInputChange("login", e)}
             type="email"
-            class="input-field"
+            className="input-field"
             placeholder="E-mail Address"
             name="email"
             required
           />
-          <label for="email">Password:</label>
+          <label htmlFor="email">Password:</label>
           <input
+            onChange={(e) => handleInputChange("login", e)}
             type="password"
-            class="input-field"
+            className="input-field"
             placeholder="Enter Password"
             name="password"
             required
           />
-          <button type="submit" class="submit-btn">
+          <input type="hidden" name="action" value="login" />
+          <button type="submit" className="submit-btn">
             Login
           </button>
           <p>
@@ -32,25 +83,41 @@ const LoginSignup = (props) => {
         </form>
 
         {/* Signup Form */}
-        <form action="" method="POST" id="signup-form" class="form">
+        <form
+          onSubmit={(e) => handleFormSubmit("signup", e)}
+          id="signup-form"
+          className="form"
+        >
           <h2>Sign Up</h2>
-          <label for="email">E-mail Address:</label>
+          <label htmlFor="email">E-mail Address:</label>
           <input
+            onChange={(e) => handleInputChange("signup", e)}
             type="email"
-            class="input-field"
+            className="input-field"
             placeholder="E-mail Address"
             name="email"
             required
           />
-          <label for="email">Password:</label>
+          <label htmlFor="fullname">Full Name:</label>
           <input
+            onChange={(e) => handleInputChange("signup", e)}
+            type="text"
+            className="input-field"
+            placeholder="Enter Name"
+            name="fullname"
+            required
+          />
+          <label htmlFor="email">Password:</label>
+          <input
+            onChange={(e) => handleInputChange("signup", e)}
             type="password"
-            class="input-field"
+            className="input-field"
             placeholder="Enter Password"
             name="password"
             required
           />
-          <button type="submit" class="submit-btn">
+          <input type="hidden" name="action" value="signup" />
+          <button type="submit" className="submit-btn">
             Sign Up
           </button>
           <p>

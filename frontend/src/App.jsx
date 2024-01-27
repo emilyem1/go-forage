54f30bc265c08b66f49ab55762dfe353294315d1
Kenny Lee <kenny.lee927@gmail.com>
2024-01-26 23:12:09 -0500

    pass setSelectedRoute to login/signup for redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, React } from "react";
+import React, { useState } from "react";
 import useApplicationData from "./hooks/useApplicationData";
 import "./App.css";
 
@@ -34,7 +34,7 @@ function App() {
       ) : selectedRoute === "FIELDJOURNAL" ? (
         <FieldJournal />
       ) : (
-        <LoginSignup />
+        <LoginSignup setSelectedRoute={setSelectedRoute} />
       )}
     </div>
   );

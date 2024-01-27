6d25feb9db3163f0dca2a085967c9dd7bed51ee6
Kenny Lee <kenny.lee927@gmail.com>
2024-01-26 21:21:25 -0500

    implement simple signup logic

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -15,8 +15,14 @@ module.exports = (db) => {
 
   router.post("/users", async (request, response) => {
     console.log("Received POST request to /users");
-    const { email, password, fullname, action } = request.body;
-    console.log("Received data:", { email, password, fullname, action });
+    const { email, password, fullname, profilePhoto, action } = request.body;
+    console.log("Received data:", {
+      email,
+      password,
+      fullname,
+      profilePhoto,
+      action,
+    });
 
     if (action === "login") {
       // Login logic
@@ -25,7 +31,16 @@ module.exports = (db) => {
     } else if (action === "signup") {
       // Signup logic
       // Create a new user in the database
-      response.json({ message: "Signup logic to be implemented" });
+      db.query(
+        `
+        INSERT INTO USER_ACCOUNT (FULLNAME, EMAIL, PASSWORD, PHOTO_URL)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+      `,
+        [fullname, email, password, profilePhoto]
+      ).then(({ rows }) => {
+        response.json(rows[0]);
+      });
     } else {
       response.status(400).json({ error: "Invalid action" });
     }

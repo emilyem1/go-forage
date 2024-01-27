2946201d0b3fe6b2e2e161cfa31e093224b8f936
emilyem1 <emilyemcmahonn@gmail.com>
2024-01-26 16:02:16 -0800

    Remove import

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-import "../styles/TopNavigationBar.scss";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 // Overiding the default colour of MUI

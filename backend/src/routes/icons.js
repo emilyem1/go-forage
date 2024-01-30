25ce588b8d2449f67a78f6aacadd9e060bbda54d
emilyem1 <emilyemcmahonn@gmail.com>
2024-01-30 14:09:04 -0800

    Remove placeholder email, replace with users email

--- backend/src/routes/icons.js
+++ backend/src/routes/icons.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 
 module.exports = (db) => {
   router.get("/icons", (request, response) => {
+    const userEmail = request.query.userEmail;
     db.query(
       `
       SELECT DISTINCT MUSHROOM.ICON
       FROM USER_ACCOUNT
       JOIN BLOG ON USER_ACCOUNT.ID = BLOG.USER_ID
       JOIN MUSHROOM ON BLOG.MUSHROOM_ID = MUSHROOM.ID
-      WHERE USER_ACCOUNT.EMAIL = 'jdoe@test.com';
-    `
+      WHERE USER_ACCOUNT.EMAIL = $1;
+    `,
+    [userEmail]
     ).then(({ rows: icons }) => {
       response.json(icons);
     });

6bddf799dba22a12bbe1c59715b60d71db021eff
Mohamed Waked <mwaked01@gmail.com>
2024-02-02 06:20:09 -0500

    clear fav-blogs on logout

--- frontend/src/components/Favourites.jsx
+++ frontend/src/components/Favourites.jsx
@@ -3,13 +3,12 @@ import "../styles/MushroomList.scss";
 
 const Favourites = (props) => {
   const { blogs, setSelectedRoute, setBlogSelected, favouriteBlogs,userData } = props;
-  const user_id= userData.user_id;
-
+  const {user_id}= userData;
   return (
     <main>
       <ul className="mushroom-list">
         
-        {user_id? blogs
+        {favouriteBlogs[user_id]? blogs
           .filter((blog) => (favouriteBlogs[user_id]).includes(blog.id))
           .map((blog) => (
             <BlogListItem

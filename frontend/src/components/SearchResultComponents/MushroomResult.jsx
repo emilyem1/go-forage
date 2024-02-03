1e21db059084458d690ea4d04bbbf7356edcc026
Kenny Lee <kenny.lee927@gmail.com>
2024-02-02 23:01:11 -0500

    minor styling and linting

--- frontend/src/components/SearchResultComponents/MushroomResult.jsx
+++ frontend/src/components/SearchResultComponents/MushroomResult.jsx
@@ -11,37 +11,47 @@ const MushroomResult = (props) => {
   const { mushroom } = props;
 
   const shortenedInfo =
-  mushroom.info.length > 100
-    ? `${mushroom.info.slice(0, 100)}...`
-    : mushroom.info;
+    mushroom.info.length > 100
+      ? `${mushroom.info.slice(0, 100)}...`
+      : mushroom.info;
 
   return (
-    <Card className="mushroom-result-item" sx={{maxWidth: 345}}>
+    <Card
+      className="mushroom-result-item"
+      sx={{
+        maxWidth: 345,
+        boxShadow: 3,
+        borderRadius: 3,
+        transition: "box-shadow 0.3s ease",
+        "&:hover": {
+          boxShadow: 5,
+        },
+      }}
+    >
       <CardMedia
-      component="img"
-      alt={mushroom.name}
-      height="140"
-      image={`/images/${mushroom.image_url}`}
-      className="mushroom-result-image"
+        component="img"
+        alt={mushroom.name}
+        height="140"
+        image={`/images/${mushroom.image_url}`}
+        className="mushroom-result-image"
       />
 
       <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-      {mushroom.title}
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-      {shortenedInfo}
-      </Typography>
-      {/* <p>Edible? {mushroom.edible ? "Yes" : "No"}</p> */}
-
+        <Typography gutterBottom variant="h5" component="div">
+          {mushroom.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {shortenedInfo}
+        </Typography>
+        {/* <p>Edible? {mushroom.edible ? "Yes" : "No"}</p> */}
       </CardContent>
-      <CardActions sx={{display: 'flex', justifyContent: 'space-between'}}>
+      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
         <Button size="small">Learn More</Button>
         <img
-        style={{ width: "22px" }}
-        className="mushroom-result-icon"
-        src={`/images/${mushroom.icon}`}
-      />
+          style={{ width: "22px" }}
+          className="mushroom-result-icon"
+          src={`/images/${mushroom.icon}`}
+        />
       </CardActions>
     </Card>
   );

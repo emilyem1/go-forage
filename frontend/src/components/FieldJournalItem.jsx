a5b4aef751ff4ec1bbb110f2bef59795cfa68f7e
emilyem1 <emilyemcmahonn@gmail.com>
2024-02-05 21:31:23 -0800

    Redo component to match BlogList/Details

--- frontend/src/components/FieldJournalItem.jsx
+++ frontend/src/components/FieldJournalItem.jsx
@@ -1,7 +1,38 @@
 import BlogListMap from "./BlogListMap";
+import BookmarkButton from "./BookmarkButton";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Avatar,
+  IconButton,
+  CardHeader,
+} from "@mui/material";
 
 const FieldJournalItem = (props) => {
-  const { blog, setSelectedRoute, setBlogSelected } = props;
+  const { blog, setSelectedRoute, setBlogSelected, onBookmarkClick,
+    bookmarkedBlogs, userData } = props;
+
+  const { user_id } = userData;
+
+  const bookmarkSelect = bookmarkedBlogs[user_id].includes(blog.id)
+  ? true
+  : false;
+
+  const dateFormatter = (blogDate) => {
+    const date = new Date(blogDate);
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    return new Intl.DateTimeFormat("en-US", options).format(date);
+  };
+
+  const shortenedInfo =
+    blog.content.length > 100
+      ? `${blog.content.slice(0, 100)}...`
+      : blog.content;
+
 
   const handleClick = () => {
     setBlogSelected(blog);
@@ -9,32 +40,59 @@ const FieldJournalItem = (props) => {
   };
 
   return (
-    <section onClick={handleClick} className="mushroom-list__item">
-      <div>
+    <Card
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      boxShadow: 3,
+      borderRadius: 3,
+      transition: "box-shadow 0.3s ease",
+      "&:hover": {
+        boxShadow: 5,
+      },
+    }}
+  >
+    <CardHeader
+      avatar={<Avatar alt={blog.username} src={blog.avatar} />}
+      action={
+        <IconButton>
+          <BookmarkButton
+            blog={blog}
+            onBookmarkClick={onBookmarkClick}
+            bookmarkSelect={bookmarkSelect ? true : false}
+            user_id={user_id}
+          />
+        </IconButton>
+      }
+      title={blog.title}
+      subheader={`By: ${blog.username} published: ${dateFormatter(
+        blog.date
+      )}`}
+    />
+    <section onClick={handleClick}>
+      <CardMedia>
         <BlogListMap location={{ lat: blog.lat, lng: blog.long }} />
-      </div>
-      <div>{blog.title}</div>
-      <div>By: {blog.username}</div>
-      {blog.mushrooms && blog.mushrooms.length > 0 && (
-        <div>
-          {blog.mushrooms.map((mushroom, index) => (
-            <div key={index}>
-              <img
-                className="mushroom-list__image"
-                src={`images/${mushroom.mushroom_image}`}
-                alt={mushroom.mushroom_name}
-              />
-              <div>{mushroom.mushroom_name}</div>
-            </div>
-          ))}
-          <div className="mushroom-list__details">
-            <footer className="mushroom-list__info">
-              <div className="mushroom-list__description">{blog.content}</div>
-            </footer>
-          </div>
-        </div>
-      )}
+      </CardMedia>
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {shortenedInfo}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Button size="small">Read More</Button>
+        {blog.mushrooms.map((mushroom, index) => (
+              <div key={index}>
+                <img
+                  style={{ width: "22px" }}
+                  src={`images/${mushroom.mushroom_icon}`}
+                  alt={mushroom.mushroom_name}
+                />
+              </div>
+            ))}
+      </CardActions>
     </section>
+    </Card>
   );
 };
 

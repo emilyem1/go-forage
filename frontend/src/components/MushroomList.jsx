b3d312f69df5a52c35036c6ad328f384b318556f
emilyem1 <emilyemcmahonn@gmail.com>
2024-02-06 22:26:32 -0800

    Add MushroomCard, add styling

--- frontend/src/components/MushroomList.jsx
+++ frontend/src/components/MushroomList.jsx
@@ -1,15 +1,30 @@
 import MushroomListItem from "./MushroomListItem";
 import "../styles/MushroomList.scss";
+import Stack from '@mui/material/Stack';
+import MushroomCard from "./MushroomCard";
 
 const MushroomList = (props) => {
-  const { mushrooms } = props;
+  const { mushrooms, users } = props;
+
   return (
     <main>
-      <ul className="mushroom-list">
+      <div class='card'style={{ display: 'flex', marginLeft: '10%', marginTop: '1%', position: 'sticky', top: '0'}}>
+      <MushroomCard 
+        fullname={users.fullname} 
+        email={users.email} 
+        profilePhoto={users.profilePhoto} 
+      />
+      </div>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        marginTop="-14%"
+      >
         {mushrooms.map((mushroom) => (
           <MushroomListItem className="mushroom-list" key={mushroom.id} mushroom={mushroom} />
         ))}
-      </ul>
+      </Stack>
     </main>
   );
 };

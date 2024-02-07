1779d290cc360709ede7f2ebea8b0fb4d7fa5769
emilyem1 <emilyemcmahonn@gmail.com>
2024-02-07 14:11:27 -0800

    Add speeddail and modul for blogform, add theme styling

--- frontend/src/components/BlogList.jsx
+++ frontend/src/components/BlogList.jsx
@@ -1,13 +1,16 @@
 import BlogListItem from "./BlogListItem";
 import Bookmarks from "./Bookmarks";
 
+import EditIcon from '@mui/icons-material/Edit';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+
 // import "../styles/MushroomList.scss";
 import BlogForm from "./BlogForm";
-
 import { useState } from "react";
-import Switch from "@mui/material/Switch";
-import Collapse from "@mui/material/Collapse";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Modal } from "@mui/material";
+import { ThemeProvider } from '@mui/material/styles'; 
+// Must use this to wrap html to pass in theme
 
 const BlogList = (props) => {
   const {
@@ -20,89 +23,115 @@ const BlogList = (props) => {
     userData,
     onBookmarkClick,
     setUserSelected,
+    theme,
   } = props;
-
-  const [checked, setChecked] = useState(false);
   const [feed, setFeed] = useState("HOME");
-  const handleChange = () => {
-    setChecked((prev) => !prev);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 1000,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
   };
 
   return (
-    <main>
-      Add a Blog:{<Switch checked={checked} onChange={handleChange} />}
-      <div>
-        <Collapse in={checked}>
-          <BlogForm mushrooms={mushrooms} setBlogUpdate={setBlogUpdate} />
-          <br />
-        </Collapse>
-      </div>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-evenly",
-        }}
+    <ThemeProvider theme={theme}>
+      <main>
+      <Box sx={{ transform: 'translateZ(0px)', flexGrow: 1, display: 'flex', marginLeft: '1%', marginTop: '1%', position: 'sticky', top: '0' }}>
+      <SpeedDial
+        ariaLabel="SpeedDial"
+        sx={{ position: 'absolute', bottom: -100, right: 100 }}
+        icon={<SpeedDialIcon openIcon={<EditIcon />} onClick={handleOpen} />}
       >
-        <Button
-          onClick={() => {
-            setFeed("HOME");
+      </SpeedDial>
+    </Box>
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <BlogForm mushrooms={mushrooms} setBlogUpdate={setBlogUpdate} handleClose={handleClose} setOpen={setOpen} />
+        </Box>
+      </Modal>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            margin: "1% 0"
           }}
-          variant="contained"
         >
-          Home Feed
-        </Button>
-
-        <Button
-          onClick={() => {
-            setFeed("FAVOURITES");
-          }}
-          variant="contained"
-        >
-          Favourite Blogs
-        </Button>
-      </Box>
-      
-      {feed === "HOME" && (
-        <div>
-
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignContent:"center",
-              flexWrap: "wrap",
-              padding:"10px",
-              gap: "2rem",
+          <Button
+            onClick={() => {
+              setFeed("HOME");
             }}
+            variant="contained"
+            color="primary"
           >
-            {blogs.map((blog) => (
-              <BlogListItem
-                className="mushroom-list"
-                key={blog.id}
-                blog={blog}
-                setBlogSelected={setBlogSelected}
-                setSelectedRoute={setSelectedRoute}
-                bookmarkedBlogs={bookmarkedBlogs}
-                userData={userData}
-                onBookmarkClick={onBookmarkClick}
-                setUserSelected={setUserSelected}
-              />
-            ))}
-          </Box>
-        </div>
-      )}
-      {feed === "FAVOURITES" && (
-        <Bookmarks
-          blogs={blogs}
-          setBlogSelected={setBlogSelected}
-          setSelectedRoute={setSelectedRoute}
-          bookmarkedBlogs={bookmarkedBlogs}
-          userData={userData}
-          onBookmarkClick={onBookmarkClick}
-          setUserSelected={setUserSelected}
-        />
-      )}
-    </main>
+            Home Feed            
+          </Button>
+
+          <Button
+            onClick={() => {
+              setFeed("FAVOURITES");
+            }}
+            variant="contained"
+          >
+             Favourite Blogs
+          </Button>
+        </Box>
+        
+        {feed === "HOME" && (
+          <div>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignContent:"center",
+                flexWrap: "wrap",
+                padding:"10px",
+                gap: "2rem",
+              }}
+            >
+              {blogs.map((blog) => (
+                <BlogListItem
+                  className="mushroom-list"
+                  key={blog.id}
+                  blog={blog}
+                  setBlogSelected={setBlogSelected}
+                  setSelectedRoute={setSelectedRoute}
+                  bookmarkedBlogs={bookmarkedBlogs}
+                  userData={userData}
+                  onBookmarkClick={onBookmarkClick}
+                  setUserSelected={setUserSelected}
+                />
+              ))}
+            </Box>
+          </div>
+        )}
+        {feed === "FAVOURITES" && (
+          <Bookmarks
+            blogs={blogs}
+            setBlogSelected={setBlogSelected}
+            setSelectedRoute={setSelectedRoute}
+            bookmarkedBlogs={bookmarkedBlogs}
+            userData={userData}
+            onBookmarkClick={onBookmarkClick}
+            setUserSelected={setUserSelected}
+          />
+        )}
+      </main>
+    </ThemeProvider>
   );
 };
 
--- frontend/src/components/FieldJournal.jsx
+++ frontend/src/components/FieldJournal.jsx
@@ -1,16 +1,28 @@
 import FieldJournalItem from "./FieldJournalItem";
 import BlogForm from "./BlogForm";
 import { useState, useEffect } from "react";
-import Switch from "@mui/material/Switch";
-import Collapse from "@mui/material/Collapse";
-import { Box, Button } from "@mui/material";
+import { Box, Modal } from "@mui/material";
+import { ThemeProvider } from '@mui/material/styles'; 
+import EditIcon from '@mui/icons-material/Edit';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 
 const FieldJournal = (props) => {
-  const { email, setBlogSelected, setSelectedRoute, mushrooms, onBookmarkClick, bookmarkedBlogs, userData, setBlogUpdate } = props;
+  const { email, setBlogSelected, setSelectedRoute, mushrooms, onBookmarkClick, bookmarkedBlogs, userData, setBlogUpdate, theme } = props;
   const [blogs, setBlogs] = useState([]);
-  const [checked, setChecked] = useState(false);
-  const handleChange = () => {
-    setChecked((prev) => !prev);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 1000,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
   };
 
   useEffect(() => {
@@ -28,14 +40,26 @@ const FieldJournal = (props) => {
   }, [email]);
 
   return (
+    <ThemeProvider theme={theme}>
     <main>
-      Add a Blog:{<Switch checked={checked} onChange={handleChange} />}
-      <div>
-        <Collapse in={checked}>
-          <BlogForm mushrooms={mushrooms} setBlogUpdate={setBlogUpdate} />
-          <br />
-        </Collapse>
-      </div>
+    <Box sx={{ transform: 'translateZ(0px)', flexGrow: 1, display: 'flex', marginLeft: '1%', marginTop: '1%', position: 'sticky', top: '0' }}>
+      <SpeedDial
+        ariaLabel="SpeedDial"
+        sx={{ position: 'absolute', bottom: -100, right: 100 }}
+        icon={<SpeedDialIcon openIcon={<EditIcon />} onClick={handleOpen} />}
+      >
+      </SpeedDial>
+    </Box>
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <BlogForm mushrooms={mushrooms} setBlogUpdate={setBlogUpdate} handleClose={handleClose} setOpen={setOpen} />
+        </Box>
+      </Modal>
       <Box
         sx={{
           display: "flex",
@@ -61,6 +85,7 @@ const FieldJournal = (props) => {
         ))}
       </Box>
     </main>
+    </ThemeProvider>
   );
 };
 

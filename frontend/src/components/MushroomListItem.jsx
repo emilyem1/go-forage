84a4f4a35c46a2cfc19de1bc0d896c36e95ccdca
Kenny Lee <kenny.lee927@gmail.com>
2024-02-06 22:20:23 -0500

    fix incorrect function call

--- frontend/src/components/MushroomListItem.jsx
+++ frontend/src/components/MushroomListItem.jsx
@@ -4,7 +4,7 @@ const MushroomListItem = (props) => {
   const { mushroom, setMushroomSelected, setSelectedRoute } = props;
 
   const handleClick = () => {
-    setMushroomSelected({mushroom})
+    setMushroomSelected(mushroom)
     setSelectedRoute("MUSHROOMDETAILS");
   };
 
